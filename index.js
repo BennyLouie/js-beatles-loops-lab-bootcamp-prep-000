8b14c024193ec4d83f5098111134389593a8272b
Benny Louie <bennylouie1412@gmail.com>
2019-06-14 19:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var whoPlaysWhat= []
+  var n = musicians.length
+  for (let i = 0; i < n; i+=1) {
+    whoPlaysWhat.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return whoPlaysWhat
+}
+
+function johnLennonFacts(facts) {
+  var theFacts = []
+  var n = facts.length
+  while (n > 0) {
+    theFacts.unshift(`${facts[n-=1]}!!!`)
+  }
+  return theFacts
+}
+
+function iLoveTheBeatles(num) {
+  var i = 0
+  function incrementVariable() {
+    i += 1
+    return i
+  }
+  var shoutOut = []
+  do {
+    `I love the Beatles!`}
+  while (i < 15)
+}
